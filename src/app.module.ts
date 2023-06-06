20877ea8b265f020d7fd9e7308136e93196e5a7d
MaksimGolodov <maksim.golodov@digitaltwin.ru>
2023-06-06 12:36:14 +0300

    ADD: all exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { ProvidersModule } from '@lib/providers';
 import { Module } from '@nestjs/common';
+import { SharedModule } from 'lib/shared';
 
 @Module({
-  imports: [ProvidersModule],
+  imports: [SharedModule, ProvidersModule],
   controllers: [],
   providers: [],
 })
