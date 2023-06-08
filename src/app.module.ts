0fc0a3f4bd4fd523349d073ff74101e4631992e3
MaksimGolodov <maksim.golodov@digitaltwin.ru>
2023-06-08 17:31:27 +0300

    ADD: domain module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { ProvidersModule } from '@lib/providers';
 import { Module } from '@nestjs/common';
 import { SharedModule } from 'lib/shared';
 import { ApiModule } from './api';
+import { DomainsModule } from './domains/domains.module';
 
 @Module({
-  imports: [SharedModule, ProvidersModule, ApiModule],
+  imports: [SharedModule, ProvidersModule, ApiModule, DomainsModule],
   controllers: [],
   providers: [],
 })
