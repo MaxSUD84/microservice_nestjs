2474ec39512a0189258b04d8422bbdf93e1cd973
MaksimGolodov <maksim.golodov@digitaltwin.ru>
2023-06-16 14:18:18 +0300

    ADD: jwt strategy

--- libs/auth/src/auth.service.ts
+++ libs/auth/src/auth.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async validateUser(email: string) {
+    return email === 'test@mail.ru';
+    // на реальном проекте реализуется очередь с запросом проверки наличия такого пользователя в базе
+  }
+}
