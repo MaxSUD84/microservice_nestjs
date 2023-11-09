7fd3eb8c031b2f1d9255fa289d4e4b52208aa7e4
MaksimGolodov <maksim.golodov@digitaltwin.ru>
2023-11-09 17:06:15 +0300

    ADD: paginated response for graphql

--- libs/shared/src/dto/pagination.dto.ts
+++ libs/shared/src/dto/pagination.dto.ts
@@ -1,16 +1,26 @@
+import { ArgsType, Field, Int } from '@nestjs/graphql';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsNumber, IsOptional, IsPositive, Min } from 'class-validator';
 
+@ArgsType()
 export class PaginationDto {
+  @ApiPropertyOptional({ description: 'Пропуск строк', type: 'number' })
   @IsOptional()
   @Min(0)
   @IsNumber({ allowNaN: false, allowInfinity: false })
   @Type(() => Number)
+  @Field(() => Int, { description: 'Пропуск строк' })
   offset = 0;
 
+  @ApiPropertyOptional({
+    description: 'Количество отображаемых строк',
+    type: 'number',
+  })
   @IsOptional()
   @IsNumber({ allowNaN: false, allowInfinity: false })
   @Type(() => Number)
   @IsPositive()
+  @Field(() => Int, { description: 'Количество отображаемых строк' })
   limit = 15;
 }
--- src/api/resolvers/post/post.resolver.ts
+++ src/api/resolvers/post/post.resolver.ts
@@ -1,12 +1,28 @@
 import { Resolver, Query, Args } from '@nestjs/graphql';
-import { PostResponse } from '../responses/post.response';
+import { PostResponse, PaginatedPosts } from '../responses';
 import { PostFacade } from 'lib/post/application-services';
+import { PaginationDto } from 'lib/shared';
+import { plainToClass } from 'class-transformer';
 
 @Resolver(() => PostResponse)
 export class PostResolver {
   constructor(private readonly postFacade: PostFacade) {}
+
   @Query(() => PostResponse, { name: 'postById' })
   async getPostById(@Args('id') id: string) {
     return this.postFacade.queries.getOnePost(id);
+  }
+
+  @Query(() => PaginatedPosts, { name: 'posts' })
+  async getPosts(@Args() paginationDto: PaginationDto) {
+    const pagination = plainToClass(PaginationDto, paginationDto);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //  @ts-ignore
+    const [data, total] = await this.postFacade.queries.getAllPosts(pagination);
+    return {
+      ...pagination,
+      data,
+      total,
+    };
   }
 }
